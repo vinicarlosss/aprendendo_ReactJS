0a801b0b737b4d7ece81fe5341ee358a236e1321
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-01-06 20:03:08 -0300

    passando funções para componentes

--- criando-componentes/src/componentes/Corpo.js
+++ criando-componentes/src/componentes/Corpo.js
@@ -2,19 +2,32 @@ import React from 'react';
 import Dados from './Dados';
 
 export default function Corpo(){
-    
-    const cnl = 'CFB Cursos';
-    const yt = 'youtube.com/cfbcursos';
-    const crs = 'React.js';
+
+    const cnl = () => {
+        return 'CFB Cursos'
+    }
+    const yt = () =>{
+        return 'youtube.com/cfbcursos' 
+    } 
+    const crs = () => {
+        return 'React.js'
+    } 
+
+    const somar = (v1, v2) =>{
+        return v1 + v2
+    }
 
     return(
         <section>
             <h2>Curso de React</h2>
             <p>Se inscreva em nosso canal</p>
             <p>Ative o sininho e clique no joinha</p>
-            <Dados canal= {cnl}
-      youtube= {yt} 
-      curso={crs}/>
+            <Dados 
+                canal= {cnl}
+                youtube= {yt} 
+                curso={crs}
+                somar = {somar}
+            />
         </section>
     )
 }
