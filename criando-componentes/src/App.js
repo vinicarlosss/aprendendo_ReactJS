4b0667c0daf54c66899571d7db9fea3c4684c358
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-01-06 19:54:55 -0300

    aprendendo a usar props nos componentes

--- criando-componentes/src/App.js
+++ criando-componentes/src/App.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import Header from './componentes/Header.js';
 import Corpo from './componentes/Corpo.js';
+import Dados from './componentes/Dados';
 
 function App() {
   return (
--- criando-componentes/src/componentes/Corpo.js
+++ criando-componentes/src/componentes/Corpo.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Dados from './Dados';
 
 export default function Corpo(){
+    
+    const cnl = 'CFB Cursos';
+    const yt = 'youtube.com/cfbcursos';
+    const crs = 'React.js';
+
     return(
         <section>
             <h2>Curso de React</h2>
             <p>Se inscreva em nosso canal</p>
             <p>Ative o sininho e clique no joinha</p>
+            <Dados canal= {cnl}
+      youtube= {yt} 
+      curso={crs}/>
         </section>
     )
 }
