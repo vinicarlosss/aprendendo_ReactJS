65d12bda32e39da0a3f34ced5d4b0a3051778d79
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-01-09 21:27:50 -0300

    aprendendo renderização de componentes

---
+++ renderizando-componentes/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Relogio from './Componentes/Relogio/Relogio';
+import Titulo from './Componentes/Titulo/Titulo';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Relogio/>
+      <Titulo/>
+    </>
+  );
+}
+
+export default App;
---
+++ renderizando-componentes/src/Componentes/Titulo/Titulo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Titulo(){
+    return(
+        <h1>Olá, seja Bem vindo</h1>
+    )
+}
