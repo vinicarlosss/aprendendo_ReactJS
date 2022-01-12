69e793efeecc04a608c567604c5d2b9a6e223159
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-01-12 03:39:20 -0300

    aprendendo a usar condicionais na renderização de componentes

---
+++ aprendendendo-renderizacao-condicional/src/App.js
@@ -0,0 +1,35 @@
+import React,{useState} from "react"
+
+function App() {
+
+  const [log,setLog] = useState(false);
+  
+  const msglogin =()=>{
+    return 'Usuário Logado'
+  }
+
+  const msglogoff =()=>{
+    return 'Favor Logar'
+  }
+
+const cumprimento =() =>{
+  const hora = new Date().getHours()
+  if(hora >= 0 && hora < 13){
+    return <p>Bom dia</p>
+  }else if(hora >= 13 && hora < 18){
+    return <p>Boa tarde</p>
+  }else{
+    return <p>Boa noite</p>
+  }
+}
+
+  return (
+    <>
+      {cumprimento()}
+      <p>{log?msglogin():msglogoff()}</p>
+      <button onClick={()=>setLog(!log)}>{log?'Logoff':'Login'}</button>
+    </>
+  );
+}
+
+export default App;
