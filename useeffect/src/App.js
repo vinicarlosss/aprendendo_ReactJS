30ef29fef9efaf7f72387c36355159207ae95cd3
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-01-17 18:39:27 -0300

    aprendendo a usar o useEffect

---
+++ useeffect/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect,useState } from "react";
+
+function App() {
+
+  const[contagem,setContagem]=useState(0)
+
+  useEffect(
+    ()=>{
+      console.log("Página carregada")
+      document.title= 'Contagem: '+contagem
+    }
+  )
+
+  return (
+    <>
+      <p>Contagem: {contagem}</p>
+      <button onClick={()=>setContagem(contagem+1)}>Contar</button>
+    </>
+  );
+}
+
+export default App;
