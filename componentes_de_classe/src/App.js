48bdf8ab951cc452f1ed66ed2a7de1d3358bbf9d
unknown <carlos.viniciusufrpe@gmail.com>
2022-01-28 20:47:05 -0300

    aprendendo a trabalhar com componentes de classe em react

---
+++ componentes_de_classe/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Classe from "./Componentes/Classe";
+
+function App() {
+  return (
+    <>
+      <Classe canal="CFB Cursos"/>
+    </>
+  );
+}
+
+export default App;
---
+++ componentes_de_classe/src/Componentes/Classe.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Classe extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <>
+                <h1>Primeiro Componente de Classe</h1>
+                <p>Canal: {this.props.canal}</p>
+            </>
+        )
+    }
+}
+export default Classe
